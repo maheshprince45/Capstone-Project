2def575a05efa4f81d96059dd345ec586fd936eb
maheshprince45 <153383990+maheshprince45@users.noreply.github.com>
2025-08-29 15:03:05 +0530

    Update variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,9 +1,10 @@
 variable "project"                { default = "financeme" }
-variable "region"                 { default = "us-east-1" }
+variable "region"                 { default = "us-east-1a" }
 variable "vpc_cidr"               { default = "10.0.0.0/16" }
 variable "public_subnet_cidr"     { default = "10.0.1.0/24" }
 variable "instance_type"          { default = "t3.medium" }
 variable "allowed_ssh_cidr"       { default = "0.0.0.0/0" } # tighten to YOUR_IP/32 in real use
 variable "ssh_key_name"           { default = "AWS-Basic" }
+
 
 
