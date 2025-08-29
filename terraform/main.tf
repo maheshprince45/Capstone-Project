ac5c0a3b3478444cb7ba9ffd393654d11b595196
maheshprince45 <153383990+maheshprince45@users.noreply.github.com>
2025-08-29 14:12:46 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -110,5 +110,4 @@ resource "aws_instance" "ubuntu_host" {
   tags = merge(local.tags, { Name = "${var.project}-ubuntu-k8s" })
 }
 
-output "ec2_public_ip"  { value = aws_instance.ubuntu_host.public_ip }
-output "ec2_public_dns" { value = aws_instance.ubuntu_host.public_dns }
+
