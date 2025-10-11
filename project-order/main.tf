206869be2653b9c0e14254b01e2c7cbe4d981e55
maheshprince45 <153383990+maheshprince45@users.noreply.github.com>
2025-10-11 20:27:26 +0200

    Update main.tf

--- project-order/main.tf
+++ project-order/main.tf
@@ -1,7 +1,6 @@
 
 module "vpc" {
   source          = "s3::https://s3.amazonaws.com/my-terraform-modules-bucket-dish/vpc"
-"
   project         = var.name
   vpc_cidr        = var.vpc_cidr
   public_subnet_cidr = var.public_subnets[0]
@@ -20,7 +19,7 @@ module "sg" {
 
 module "ec2" {
   source        = "s3::https://s3.amazonaws.com/my-terraform-modules-bucket-dish/ec2"
-  project       = "${var.name}-web"s3://my-terraform-modules-bucket-dish/ec2/
+  project       = "${var.name}-web"
   ami           = "ami-0261755bbcb8c4a84"
   instance_type = var.instance_type
   subnet_id     = module.vpc.subnet_id
