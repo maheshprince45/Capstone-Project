291532e2932e117fada98690e2938642dfa8a3b9
maheshprince45 <153383990+maheshprince45@users.noreply.github.com>
2025-10-11 07:48:32 +0200

    Update variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,10 +1,11 @@
-variable "project"                { default = "Banking" }
+variable "project"                { default = "financeme" }
 variable "region"                 { default = "us-east-1" }
 variable "vpc_cidr"               { default = "10.0.0.0/16" }
 variable "public_subnet_cidr"     { default = "10.0.1.0/24" }
 variable "instance_type"          { default = "t3.small" }
 variable "allowed_ssh_cidr"       { default = "0.0.0.0/0" } # tighten to YOUR_IP/32 in real use
 variable "ssh_key_name"           { default = "Capstone" }
+
 
 
 
