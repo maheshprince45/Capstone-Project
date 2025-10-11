32fd5903597241f2327414c5e6a05b7e070c72f5
maheshprince45 <153383990+maheshprince45@users.noreply.github.com>
2025-10-11 20:09:15 +0200

    Update variables.tf

--- project-order/variables.tf
+++ project-order/variables.tf
@@ -2,27 +2,37 @@ variable "aws_region" {
   description = "AWS region"
   default     = "us-east-1"
 }
+
 variable "aws_profile" {
   description = "AWS CLI profile"
   default     = "default"
 }
+
 variable "name" {
   description = "Project name"
   default     = "dish"
 }
+
 variable "vpc_cidr" {
   description = "CIDR for VPC"
   default     = "10.10.0.0/16"
 }
+
 variable "public_subnets" {
-  default = ["10.10.1.0/24", "10.10.2.0/24"]
+  type        = list(string)
+  default     = ["10.10.1.0/24", "10.10.2.0/24"]
 }
+
 variable "azs" {
-  default = ["us-east-1a", "us-east-1b"]
+  type        = list(string)
+  default     = ["us-east-1a", "us-east-1b"]
 }
+
 variable "instance_type" {
-  default = "t3.micro"
+  default     = "t3.micro"
 }
+
 variable "key_name" {
-  default = null
+  description = "Name of your EC2 key pair"
+  default     = null
 }
