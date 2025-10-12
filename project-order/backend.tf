245d6e79af553c881c26152c8af9c354495af90a
maheshprince45 <153383990+maheshprince45@users.noreply.github.com>
2025-10-12 06:53:40 +0200

    Update backend.tf

--- project-order/backend.tf
+++ project-order/backend.tf
@@ -1,1 +1,10 @@
+terraform {
+  backend "s3" {
+    bucket         = "my-tf-state-bucket-terraform-works"
+    key            = "capstone-project/terraform.tfstate"
+    region         = "us-east-1"
+    dynamodb_table = "terraform-lock-works"
+    encrypt        = true
+  }
+}
 
